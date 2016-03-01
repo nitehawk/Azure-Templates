05b2c6ca16028405f46266ecced7a7fd3ed1b75e
Matthew <nite97m@gmail.com>
2016-03-01 12:47:24 -0600

    Add launch script to simplify deploying the template - maybe break into 3 scripts (groupcreate, deployment, destroy?)

---
+++ sge-on-sles12/launch.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Create resource group and launch template
+#
+# Assume we have a valid login, but verify we are in proper mode
+
+azure config mode arm
+
+# Create group
+GROUP=${1-democluster}
+SITE=${2-West US}
+azure group create -n $GROUP -l "$SITE"
+
+# Create deployment
+azure group deployment create --template-uri https://raw.githubusercontent.com/nitehawk/Azure-Templates/master/sge-on-sles12/azuredeploy.json -g $GROUP -e ../../democlusuter.parameters.json -n demodeployment
